e0a4635b17e77667cb25692c268ca8fe970db179
Fitruscus <adolfovera97@gmail.com>
2025-11-06 20:31:55 -0400

    controllers y models

--- TareaApii/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TareaApii.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
